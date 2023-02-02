2730abbc518f9a20f63e757f4f2186fc285e00ad
Draqqonfly <dragovonnova@gmail.com>
2023-02-02 18:47:53 +0100

    Test fix

--- app/src/test/java/com/moonlightbutterfly/cryptohub/presentation/signin/SignInFacadeTest.kt
+++ app/src/test/java/com/moonlightbutterfly/cryptohub/presentation/signin/SignInFacadeTest.kt
@@ -1,10 +1,6 @@
 package com.moonlightbutterfly.cryptohub.presentation.signin
 
-import com.moonlightbutterfly.cryptohub.usecases.EmailSignInUseCase
-import com.moonlightbutterfly.cryptohub.usecases.FacebookSignInUseCase
 import com.moonlightbutterfly.cryptohub.usecases.GoogleSignInUseCase
-import com.moonlightbutterfly.cryptohub.usecases.PhoneSignInUseCase
-import com.moonlightbutterfly.cryptohub.usecases.TwitterSignInUseCase
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
@@ -14,17 +10,9 @@ import org.junit.Test
 class SignInFacadeTest {
 
     private val googleSignInUseCase: GoogleSignInUseCase = mockk()
-    private val facebookSignInUseCase: FacebookSignInUseCase = mockk()
-    private val twitterSignInUseCase: TwitterSignInUseCase = mockk()
-    private val emailSignInUseCase: EmailSignInUseCase = mockk()
-    private val phoneSignInUseCase: PhoneSignInUseCase = mockk()
 
     private val signInFacade = SignInFacade(
         googleSignInUseCase,
-        emailSignInUseCase,
-        phoneSignInUseCase,
-        facebookSignInUseCase,
-        twitterSignInUseCase
     )
 
     @Before
